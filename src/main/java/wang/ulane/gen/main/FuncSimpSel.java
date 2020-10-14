3cb918b9ee91e456d2c5d92115c92e8e7e5904a8
ULANE\eshonulane <eshonulane@outlook.com>
2020-10-14 16:04:23 +0800

    不依赖lombok

--- src/main/java/wang/ulane/gen/main/FuncSimpSel.java
+++ src/main/java/wang/ulane/gen/main/FuncSimpSel.java
@@ -2,19 +2,40 @@ package wang.ulane.gen.main;
 
 import java.util.List;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class FuncSimpSel {
 	
 	private boolean flag = false;
 	private List<String> whereFields;
 	private List<String> selectFields;
+	
+	public FuncSimpSel() {
+		super();
+	}
+	
+	public FuncSimpSel(boolean flag, List<String> whereFields, List<String> selectFields) {
+		super();
+		this.flag = flag;
+		this.whereFields = whereFields;
+		this.selectFields = selectFields;
+	}
 
+	public boolean isFlag() {
+		return flag;
+	}
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+	public List<String> getWhereFields() {
+		return whereFields;
+	}
+	public void setWhereFields(List<String> whereFields) {
+		this.whereFields = whereFields;
+	}
+	public List<String> getSelectFields() {
+		return selectFields;
+	}
+	public void setSelectFields(List<String> selectFields) {
+		this.selectFields = selectFields;
+	}
+	
 }
